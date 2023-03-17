014cc0ab5ee6b352e6eb99228fb4979d23aac2c9
lhh-pi <2109055954@qq.com>
2023-03-17 10:53:33 +0800

    update

--- de_iter.py
+++ de_iter.py
@@ -1,7 +1,12 @@
+import argparse
 import os
-import shutil
 
-folder_path = './imgs/500'
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('-f', "--folder_path", type=str, default='')
+
+opt = parser.parse_args()
+
+folder_path = opt.folder_path
 
 for filename in os.listdir(folder_path):
     # 获取文件的绝对路径
